b2ff3b13e2daf3f89cb4e6dfbab1fca93eb2404b
Michael O'Ryan <michael.oryan@gmail.com>
2017-02-23 16:31:58 +1100

    Create palindrome.cpp

---
+++ palindrome.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <iterator>
+#include <algorithm>
+#include <functional>
+#include <cctype>
+
+int main()
+{
+  std::string words;
+  std::cout << "Enter possible palindrome: ";
+  getline (std::cin, words);
+  std::string no_punc;
+  std::copy_if(words.begin(), words.end(), std::back_insert_iterator<std::string>(no_punc), [](char c){return !ispunct(c);});
+    if(std::equal(no_punc.begin(), no_punc.end(), no_punc.rbegin()))
+        std::cout << "\"" << words << "\" " << "IS a palindrome" << std::endl;
+    else 
+       std::cout << "\"" << words << "\" " << "is NOT a palindrome" << std::endl;
+}
