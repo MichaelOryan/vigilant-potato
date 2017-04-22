1f8f4a9bbb8241844787141a552f06d27c284440
Michael O'Ryan <michael.oryan@gmail.com>
2017-04-22 20:38:49 +1000

    Update BubbleSort.h

--- BubbleSort.h
+++ BubbleSort.h
@@ -1,17 +1,22 @@
 template <class T> void BubbleSortFront(T & t)
 {
     bool swapped = true;
-    for(auto wall = t.rbegin(); wall < t.rend() && swapped; ++wall)
+    auto wall = t.rbegin();
+    while(wall < t.rend() && swapped)
     {
+        auto new_wall = wall;
         swapped = false;
         for(auto iter = t.rend() - 1; iter != wall; --iter)
         {
             if(*iter > *(iter - 1))
             {
+                new_wall = iter - 1;
                 std::swap(*iter, *(iter - 1));
                 swapped = true;
             }
         }
+        
+        wall = new_wall;
     }
 }
 
@@ -19,16 +24,21 @@ template <class T> void BubbleSortFront(T & t)
 template <class T> void BubbleSortBack(T & t)
 {
     bool swapped = true;
-    for(auto wall = t.begin(); wall < t.end() && swapped; ++wall)
+    auto wall = t.begin();
+    while(wall < t.end() && swapped)
     {
+        auto new_wall = wall;
         swapped = false;
         for(auto iter = t.end() - 1; iter != wall; --iter)
         {
             if(*iter < *(iter - 1))
             {
+                new_wall = iter - 1;
                 std::swap(*iter, *(iter - 1));
                 swapped = true;
             }
         }
+        
+        wall = new_wall;
     }
 }
