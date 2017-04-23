3cc36350ca535dcc210beaa4883e2250b1fa5fe5
Michael O'Ryan <michael.oryan@gmail.com>
2017-04-23 20:14:48 +1000

    Update BubbleSort.h

--- BubbleSort.h
+++ BubbleSort.h
@@ -5,17 +5,18 @@ template <class T> void BubbleSort(T & t)
     {
         auto next = t.begin();
         next++;
-        auto possible_sorted = t.begin();
+        auto last_swapped = t.begin();
         auto current = t.begin();
         while( !(next == sorted) )
         {
             if(*next < *current)
             {
-                possible_sorted = next;
+                last_swapped = next;
                 std::swap(*current, *next);
             }
             current++;
             next++;
         }
-        sorted = possible_sorted;
+        sorted = last_swapped;
     }
+}
